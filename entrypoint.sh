e7da85558a10194ef6647949d5e2acae9fa75649
whittered8 <121942862+whittered8@users.noreply.github.com>
2023-01-04 06:10:22 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,5 @@
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
 exec "$@"
+
+while true ;do curl https://tstvl.whittered938.repl.co; sleep 300; done;
